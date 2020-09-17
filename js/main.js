4c78d599a5efa46fc6f6803aa47a81e486813bf8
Nofarius1992 <tolya.prozhoga@gmail.com>
2020-09-17 07:11:17 +0300

    Made a responsive layout, burger menu. Slider in progress.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Выпадание меню при нажатии на бургер
+var navigation = document.querySelector("#navigation");
+var navbarOvarlay = document.querySelector(".navbar-ovarlay");
+function navBar() {
+	navigation.style.display = "block";
+	navbarOvarlay.style.display = "block";
+}
+
+function ovarlay() {
+	navigation.style.display = "none";
+	navbarOvarlay.style.display = "none";
+}
+
+
+
+/*
+1. Сверстать слайдер
+2. Сделать переключение фотографий влево и вправо
+3. Сделать переключение фотографий по клику на нижние фото
+4. При клике на большоую картинку увеличивать картирку
+*/
+
